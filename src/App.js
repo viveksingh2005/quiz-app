849d329b1f4a09180bad86fc6efffeb02d50dc6d
viveksingh2005 <chonkervivek@gmail.com>
2025-09-05 05:59:03 +0530

    Fix: remove nested Router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-
 // Pages
 import Quiz from "./pages/Quiz";
 import Results from "./pages/Results";
